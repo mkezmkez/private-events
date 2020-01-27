0ceae56d4a6a5eb6ba437f6cd814bbf4a3efe123
Luis Puente <puente.luism@gmail.com>
2020-01-27 16:56:51 -0600

    add relations to user and event models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-    has_many :created_events, foreign_key: :creator_id, class_name: "Event"
     validates :name, :email, :username, presence:true
+
+    has_many :attendances, foreign_key: :attendee_id
+    has_many :attended_events, through: :attendances, source: :attended_event
+    has_many :created_events, foreign_key: :creator_id, class_name: "Event"
 end
